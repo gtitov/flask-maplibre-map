34ce56e81758bd17b3782f3955ba94d87e5d417f
gman <gman>
2024-12-05 13:04:09 +0300

    first commit

---
+++ backend/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, Response
+import sqlite3
+import json
+import time
+
+
+app = Flask(__name__)
+
+DB_LOCATION = "cities_index.sqlite"
+
+
+@app.route("/cities/<year>")
+def cities_by_year(year):
+    start_time = time.time()
+    db = sqlite3.connect(DB_LOCATION)
+    db.row_factory = sqlite3.Row
+    cur = db.execute("SELECT * FROM cities WHERE year = ?", (year,))
+    cities = cur.fetchall()
+    cur.close()
+    db.close()
+    geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [row["longitude"], row["latitude"]],
+                },
+                "properties": dict(row),
+            }
+            for row in cities
+        ],
+    }
+    r = Response(
+        json.dumps(geojson, ensure_ascii=False),
+        mimetype="text/json",
+        headers={
+            "Access-Control-Allow-Origin": "*",
+        },
+    )
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return r
+
+
+@app.route("/city/<id>")
+def city_by_id(id):
+    start_time = time.time()
+    db = sqlite3.connect(DB_LOCATION)
+    db.row_factory = sqlite3.Row
+    cur = db.execute("SELECT * FROM cities WHERE id = ?", (id,))
+    city = cur.fetchone()
+    cur.close()
+    db.close()
+    r = Response(
+        json.dumps(dict(city), ensure_ascii=False),
+        mimetype="text/json",
+        headers={
+            "Access-Control-Allow-Origin": "*",
+        },
+    )
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return r
---
+++ main.js
@@ -0,0 +1,86 @@
+const map = new maplibregl.Map({
+    container: "map",
+    style:
+        "https://raw.githubusercontent.com/gtitov/basemaps/master/voyager-nolabels.json",
+    center: [0, 0],
+    zoom: 1,
+});
+
+map.on("load", () => {
+    map.addSource('cities', {
+        type: 'geojson',
+        data: "http://localhost:5000/cities/2020"
+    });
+
+    map.addLayer({
+        'id': 'cities-layer',
+        'source': 'cities',
+        'type': 'circle',
+        'paint': {
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#FFFFFF',
+            // SELECT MIN(total_points), MAX(total_points) FROM cities
+            'circle-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'total_points'],
+                50,
+                '#d7191c',
+                150,
+                '#ffffbf',
+                250,
+                '#1a9641'
+            ],
+            'circle-opacity': 0.8,
+            // SELECT DISTINCT group_name FROM cities
+            'circle-radius': [
+                "match",
+                ['get', 'group_name'],
+                'Малый город', 3,
+                'Средний город', 6,
+                'Большой город', 6,
+                'Крупный город', 8,
+                'Крупнейший город', 12,
+                0 // остальные
+            ]
+        }
+    });
+
+    map.on('click', 'cities-layer', (e) => {
+        console.log(e.features[0].properties.id)
+        fetch(`http://localhost:5000/city/${e.features[0].properties.id}`)
+            .then(response => response.json())
+            .then(cityProperties => {
+                console.log(cityProperties)
+                document.getElementById("city-details-modal").innerHTML = `<h1>${cityProperties.name}</h1>
+                            <img src="${cityProperties.emblem_url}" height="200">
+                            <h3>Численность населения</h3><h2>${cityProperties.people_count} тыс. чел</h2>
+                            <h3>Индекс качества городской среды</h3><h2>${cityProperties.total_points} / 360</h2>
+                            <hr>
+                            <h3>Жилье и прилегающие пространства</h3><h2>${cityProperties.house_points} / 60</h2>
+                            <h3>Озелененные пространства</h3><h2>${cityProperties.park_points} / 60</h2>
+                            <h3>Общественно-деловая инфраструктура</h3><h2>${cityProperties.business_points} / 60</h2>
+                            <h3>Социально-досуговая инфраструктура</h3><h2>${cityProperties.social_points} / 60</h2>
+                            <h3>Улично-дорожная</h3><h2>${cityProperties.street_points} / 60</h2>
+                            <h3>Общегородское пространство</h3><h2>${cityProperties.common_points} / 60</h2>`
+                document.getElementById("city-details-modal").showModal()
+            })
+    })
+
+    map.on('mouseenter', 'cities-layer', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'cities-layer', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+    document.getElementById("year-selector").addEventListener(
+        'change',
+        (e) => { map.getSource('cities').setData(`http://localhost:5000/cities/${e.target.value}`) }
+    )
+})
+
+
+
+
